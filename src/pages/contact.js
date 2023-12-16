0878ca0cbf96cc092ddd3280cf046b584bd602a7
Omar Mosleh <142842478+omarmoslh@users.noreply.github.com>
2023-12-16 14:23:54 +0200

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -13,14 +13,14 @@ const contact = () => {
         <meta name="description" content="any description" />
       </Head>
       <TransitionEffect />
-      <main className=" flex w-full flex-col items-center justify-center dark:text-light">
+      <main className="flex w-full flex-col items-center justify-center dark:text-light">
         <Layout className="pt-16 ">
           <AnimatedText
             text="Contact"
-            className=" mb-16 text-8xl lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl"
+            className="mb-16 text-8xl lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl"
           />
           <div className="flex flex-col items-center justify-center">
-            <p className=" flex flex-col text-center my-4 text-xl font-medium md:text-sm sm:text-xs pb-6">
+            <p className="flex flex-col text-center my-4 text-xl font-medium md:text-sm sm:text-xs pb-6">
               I'm currently open to freelance and actively looking for full-time
               job opportunties, so if you like what you've seen please don't
               hesitate to get in touch!
@@ -36,7 +36,7 @@ const contact = () => {
             
               href="mailto:omoslh@gmail.com"
               target={"_blank"}
-              className="flex items-center  bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base "
+              className="flex items-center  bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base"
             >
               Git In Touch
             </Link>
